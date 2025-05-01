8f2e0a8ea485987e42406dbc3f3e3764a25922f7
wellingtondebrito <wellingtondebritosouza@gmail.com>
2025-05-01 09:18:30 -0300

    iniciando o projeto

---
+++ src/main/java/com/futurodev/joinville/m1s10_atividade/M1s10AtividadeApplication.java
@@ -0,0 +1,13 @@
+package com.futurodev.joinville.m1s10_atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1s10AtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1s10AtividadeApplication.class, args);
+	}
+
+}
